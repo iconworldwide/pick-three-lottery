ee08f3320319503ac0c7ebb151703ceff5fce714
Petar Genov <genov1824@gmail.com>
2024-06-14 17:05:04 +0300

    update onboarding

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -5,8 +5,12 @@ import onboarding1 from '../assets/images/onboarding1.png';
 import onboarding2 from '../assets/images/onboarding2.png';
 import onboarding3 from '../assets/images/onboarding3.png';
 import onboarding4 from '../assets/images/onboarding4.png';
+import onboarding5 from '../assets/images/onboarding5.png';
+import onboarding6 from '../assets/images/onboarding6.png';
+import onboarding7 from '../assets/images/onboarding7.png';
+import onboarding8 from '../assets/images/onboarding8.png';
 
-const onboardingImages = [onboarding1, onboarding2, onboarding3, onboarding4];
+const onboardingImages = [onboarding1, onboarding2, onboarding3, onboarding4, onboarding5, onboarding6, onboarding7, onboarding8];
 
 interface OnboardingProps {
   onComplete: () => void;
