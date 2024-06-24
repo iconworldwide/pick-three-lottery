5c1e4ca58b77174436f946f5cf3e9fbdc4ab51a7
Petar Genov <genov1824@gmail.com>
2024-06-24 17:43:00 +0300

    Update app to use query params from window.location

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import "@twa-dev/sdk";
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes, NavLink, Navigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink, Navigate } from 'react-router-dom';
 import Boss from "./pages/Boss";
 import Play from "./pages/Play";
 import Earn from "./pages/Earn";
@@ -22,28 +22,24 @@ declare global {
 const AppWrapper: React.FC = () => {
   const { registerUser, user } = useUserContext();
 
-  useEffect(() => {
-    if(!user) {
-      // Register user from URL parameters if present
-      const urlParams = new URLSearchParams(window.location.search);
-      const userId = urlParams.get('user_id');
-      const username = urlParams.get('username') || '';
-      const reference = urlParams.get('reference') || '';
-    
-      if (userId && !user) {    
-        registerUser(userId, username, reference);
-      }
+  if(!user) {
+    // Register user from URL parameters if present
+    const urlParams = new URLSearchParams(window.location.search);
+    const userId = urlParams.get('user_id');
+    const username = urlParams.get('username') || '';
+    const reference = urlParams.get('reference') || '';
+  
+    if (userId && !user) {    
+      registerUser(userId, username, reference);
     }
-  }, [user, registerUser]);
+  }
 
   return null;
 };
 
-const AppContent: React.FC = () => {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
+const App: React.FC = () => {
+  const searchParams = new URLSearchParams(window.location.search);
   const queryString = searchParams.toString();
-  alert(queryString);
 
   const [isOnboardingComplete, setIsOnboardingComplete] = useState<boolean>(() => {
     const onboardingCompleted = localStorage.getItem('newOnboardingCompleted');
@@ -64,54 +60,46 @@ const AppContent: React.FC = () => {
   tg.expand();
 
   return (
-    <div className="app-container">
-      <Routes>
-        <Route path="/" element={<Navigate to={`/pick-three-lottery/play?${queryString}`} />} />
-        <Route path="/pick-three-lottery" element={<Navigate to={`/pick-three-lottery/play?${queryString}`} />} />
-        <Route path="/pick-three-lottery/play" element={<Play />} />
-        <Route path="/pick-three-lottery/boss" element={<Boss />} />
-        <Route path="/pick-three-lottery/cards" element={<Cards />} />
-        <Route path="/pick-three-lottery/earn" element={<Earn />} />
-        <Route path="/pick-three-lottery/roadmap" element={<Roadmap />} />
-      </Routes>
-      <nav className="tab-bar">
-        <NavLink to={`/pick-three-lottery/play?${queryString}`} className="tab-link">
-          <i className="fas fa-gamepad"></i>
-          <span>Play</span>
-        </NavLink>
-        <NavLink to={`/pick-three-lottery/boss?${queryString}`} className="tab-link">
-          <i className="fas fa-rocket"></i>
-          <span>Boss</span>
-        </NavLink>
-
-        <NavLink to={`/pick-three-lottery/cards?${queryString}`} className="tab-link">
-          <i className="far fa-file-image"></i>
-          <span>Cards</span>
-        </NavLink>
-
-        <NavLink to={`/pick-three-lottery/earn?${queryString}`} className="tab-link">
-          <i className="fas fa-coins"></i>
-          <span>Earn</span>
-        </NavLink>
-        <NavLink to={`/pick-three-lottery/roadmap?${queryString}`} className="tab-link">
-          <i className="fas fa-globe-americas"></i>
-          <span>Roadmap</span>
-        </NavLink>
-      </nav>
-    </div>
-  );
-};
-
-const App: React.FC = () => {
-  return (
-    <UserProvider>
-      <GameProvider>
+      <UserProvider>
+        <AppWrapper />
         <Router>
-          <AppWrapper />
-          <AppContent />
+          <div className="app-container">
+            <Routes>
+              <Route path="/" element={<Navigate to={`/pick-three-lottery/play?${queryString}`} />} />
+              <Route path="/pick-three-lottery" element={<Navigate to={`/pick-three-lottery/play?${queryString}`} />} />
+              <Route path="/pick-three-lottery/play" element={<Play />} />
+              <Route path="/pick-three-lottery/boss" element={<Boss />} />
+              <Route path="/pick-three-lottery/cards" element={<Cards />} />
+              <Route path="/pick-three-lottery/earn" element={<Earn />} />
+              <Route path="/pick-three-lottery/roadmap" element={<Roadmap />} />
+            </Routes>
+            <nav className="tab-bar">
+              <NavLink to={`/pick-three-lottery/play?${queryString}`} className="tab-link">
+                <i className="fas fa-gamepad"></i>
+                <span>Play</span>
+              </NavLink>
+              <NavLink to={`/pick-three-lottery/boss?${queryString}`} className="tab-link">
+                <i className="fas fa-rocket"></i>
+                <span>Boss</span>
+              </NavLink>
+
+              <NavLink to={`/pick-three-lottery/cards?${queryString}`} className="tab-link">
+                <i className="far fa-file-image"></i>
+                <span>Cards</span>
+              </NavLink>
+
+              <NavLink to={`/pick-three-lottery/earn?${queryString}`} className="tab-link">
+                <i className="fas fa-coins"></i>
+                <span>Earn</span>
+              </NavLink>
+              <NavLink to={`/pick-three-lottery/roadmap?${queryString}`} className="tab-link">
+                <i className="fas fa-globe-americas"></i>
+                <span>Roadmap</span>
+              </NavLink>
+            </nav>
+          </div>
         </Router>
-      </GameProvider>
-    </UserProvider>
+      </UserProvider>
   );
 }
 
